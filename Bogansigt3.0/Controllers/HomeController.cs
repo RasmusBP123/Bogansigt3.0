d851e863325e401dac31b9ac00f40043ac60d21c
Jesper Albrecht Madsen <jesp209i@edu.ucl.dk>
2020-04-29 21:46:41 +0200

    Initial functionality for adding and removing friends completed

--- Bogansigt3.0/Controllers/HomeController.cs
+++ Bogansigt3.0/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
-﻿using BogAnsigt.Storage;
+﻿using BogAnsigt.Models;
+using BogAnsigt.Storage;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Threading.Tasks;
 
 namespace Bogansigt3._0.Controllers
 {
     public class HomeController : Controller
     {
         private readonly DbStorage _dbContext;
+        private readonly UserManager<User> _userManager;
 
-        public HomeController(DbStorage dbContext)
+        public HomeController(DbStorage dbContext, UserManager<User> userManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -22,9 +29,27 @@ namespace Bogansigt3._0.Controllers
         {
             return View();
         }
-        public IActionResult People()
+        public async Task<IActionResult> People()
         {
-            return View(_dbContext.Users.ToListAsync());
+            return View( await _dbContext.Users.ToListAsync());
+        }
+        [HttpGet("Home/FriendToggle/{friendId}")]
+        public async Task<IActionResult> FriendToggle(string friendId)
+        {
+            var curUser = await _userManager.GetUserAsync(HttpContext.User);
+            if (curUser == null) return NotFound();
+            var friend = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == friendId);
+            if (friend == null) return NotFound();
+            if (!curUser.Friends.Contains(friend))
+            {
+                curUser.Friends.Add(friend);
+            }
+            else
+            {
+                curUser.Friends.Remove(friend);
+            }
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("People");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
