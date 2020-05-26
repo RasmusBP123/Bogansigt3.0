18c207b0aadf99b5617b13a11c41df6cf1bfda6a
Jesper Albrecht Madsen <jesp209i@edu.ucl.dk>
2020-05-26 14:13:46 +0200

    Not trying to make it a HTTPS connection

--- Bogansigt3.0/Startup.cs
+++ Bogansigt3.0/Startup.cs
@@ -46,9 +46,9 @@ namespace BogAnsigt
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
